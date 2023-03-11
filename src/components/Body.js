a12f56a07245030f1213825c6563d8721ea53ba6
Narasimha P L <107346073+Narasimha9271@users.noreply.github.com>
2023-03-11 18:03:11 +0530

    searchbar-(3.2/4)-commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,12 +2,21 @@ import React, { useState } from "react";
 import RestaurantCard from "./Restaurantcard";
 import { restaurantList } from "./constants";
 
+function filterData(searchInput, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant?.data?.name?.includes(searchInput)
+  );
+  return filterData;
+}
+
 const Body = () => {
-  const [searchInput, setSearchInput] = useState();
+  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [searchInput, setSearchInput] = useState("");
+
   return (
     <>
       <div className="m-2">
-        <input 
+        <input
           className="border h-8 border-black w-30"
           type="text"
           placeholder="search"
@@ -16,12 +25,20 @@ const Body = () => {
             setSearchInput(e.target.value);
           }}
         />
-        <button className=" m-1 h-8 p-1 rounded hover:shadow-md cursor-pointer bg-green-300">
+        <button
+          className=" m-1 h-8 p-1 rounded hover:shadow-md cursor-pointer bg-green-300"
+          onClick={() => {
+            const data = filterData(searchInput, restaurants);
+            setRestaurants(() => {
+              return data;
+            });
+          }}
+        >
           search
         </button>
       </div>
       <div className="flex flex-wrap">
-        {restaurantList.map((restaurant) => {
+        {restaurants.map((restaurant) => {
           return (
             <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
           );
